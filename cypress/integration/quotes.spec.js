ed3b4b47b2947e727c08038874ec6466d37f9f48
Jessica <duellje@oregonstate.edu>
2020-06-18 13:37:43 -0400

    submit test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,5 +1,5 @@
 //group several tests that are related together 
-describe('Inputs and submit button', () => {
+describe('Inputs and cancel button', () => {
 
     //single test 
     it('can navigate to the site', () => {
@@ -34,5 +34,50 @@ describe('Inputs and submit button', () => {
     it('submit button should be enabled', () => {
         cy.get('button#submitBtn').should('not.be.disabled')
         cy.get('button#submitBtn').should('be.enabled')
+    })
+
+    it('cancel button should clear form and disable submit button', () => {
+        cy.get('button#cancelBtn')
+            .click()
+        //assertions
+        cy.get('input[name=text]').should('be.empty')
+        cy.get('input[name=author]').should('be.empty')
+        cy.get('button#submitBtn').should('be.disabled')
+    })
+})
+
+describe('Submit button', () => {
+    it('can navigate to the site', () => {
+        //remote control operation
+        cy.visit('http://localhost:1234')
+
+        //assertion 
+        cy.url().should('include', 'localhost')
+    })
+
+    it('submit button should be disabled', () => {
+        cy.get('button#submitBtn').should('be.disabled')
+    })
+
+    it('can type a quote and an author for a new quote', () => {
+        cy
+            .get('input[name=text')
+            .type('A quote')
+            .should('have.value', 'A quote')
+        cy
+            .get('input[name=author]')
+            .type('An author')
+            .should('have.value', 'An author')
+    })
+
+    it('submit button should be enabled', () => {
+        cy.get('button#submitBtn').should('be.enabled')
+    })
+
+    it('Click button and have quote on screen', () => {
+        cy.get('button#submitBtn').click()
+        cy
+            .get(' ul > :last-child() > div')
+            .should('contain', 'A quote')
     })
 })
