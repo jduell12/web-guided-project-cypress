262134288e69ab2ab229f027af345c8720cf21c9
Jessica <duellje@oregonstate.edu>
2020-06-18 12:38:19 -0400

    writing first test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,11 @@
-// write tests here
+//group several tests that are related together 
+describe('Inputs and submit button', () => {
+    //single test 
+    it('can navigate to the site', () => {
+        //remote control operation
+        cy.visit('http://localhost:1234')
+
+        //assertion 
+        cy.url().should('include', 'localhost')
+    })
+})
